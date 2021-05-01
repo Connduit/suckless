ec6f9bd46d4bb06f404c1d5c4c2f224047b3dacc
connor <fuxkvalve@gmail.com>
2021-04-30 17:07:27 -0700

    st and dmenu commands were backwards

--- dwm/config.h
+++ dwm/config.h
@@ -69,8 +69,8 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_n,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,             		XK_space,  spawn,          {.v = termcmd } },
+	{ MODKEY,             		XK_n, 	   spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
