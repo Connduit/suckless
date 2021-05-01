ea6d5ab3831237c4d8a851eeb3b0ad979a6a9f49
connor <fuxkvalve@gmail.com>
2021-04-30 19:39:41 -0700

    dwmblocks patched with little modulues

---
+++ dwmblocks/config.def.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sb-battery.sh", 							5, 		1},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					30,		0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
